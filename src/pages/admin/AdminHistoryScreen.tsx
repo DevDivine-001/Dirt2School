befa730dd9fea09282342bcf6878a0ad9171bf10
Oscdivin <divineuwandu84@Gmail.com>
2023-11-29 12:21:37 +0100

    AdminHistoryScreen update

--- src/pages/admin/AdminHistoryScreen.tsx
+++ src/pages/admin/AdminHistoryScreen.tsx
@@ -1,95 +1,269 @@
+import image from "../../assets/Screenshot 2023-11-15 0834443.png";
+import { AiFillDelete } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import useDarkMode from "../../hook/useDarkMode";
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 const AdminHistoryScreen = () => {
+  const [colorTheme, setTheme]: any = useDarkMode();
+  const [darkSide, setDarkSide] = useState(
+    colorTheme === "light" ? true : false
+  );
+
+  const toggleDarMode = (checked: any) => {
+    setTheme(colorTheme);
+    console.log(setTheme);
+    setDarkSide(checked);
+  };
+
+  const boards = useSelector((state: any) => state.boards);
+  console.log("boards =", boards);
   return (
     <>
-      {/* <div className="flex w-full h-auto justify-center items-center flex-col">
-        <div className="w-[95%] h-full bg-white mt-5 rounded-md justify-between flex items-center">
-          <div className="w-[20%]  flex items-center justify-center ">
-            ID
+      <div className="flex w-full h-auto flex-col max-w-9 mx-auto p-1 flex-wrap bg-white dark:bg-[#000] rounded-lg gap-[10px] border-[silver] border-[1px]">
+        <header className="bg-[#fff] shadow-md rounded-md">
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-5 bg-[]">
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm font-bold sm:text-sm text-md md:text-sm"
+            >
+              Name
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm font-bold sm:text-sm text-md md:text-sm"
+            >
+              Date
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm font-bold sm:text-sm text-md md:text-sm"
+            >
+              Amount
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm font-bold sm:text-sm text-md md:text-sm"
+            >
+              Status
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm font-bold sm:text-sm text-md md:text-sm"
+            >
+              Action
+            </li>
+
+            <Switch
+              checked={darkSide}
+              onChange={toggleDarMode}
+              className={`${darkSide ? "bg-[#ffff]" : "bg-gray-200"}
+        relative inline-flex h-6 w-11 items-center border-[1px] border-[silver]
+        rounded-full 
+        `}
+            >
+              <span
+                className={`${darkSide ? "translate-x-6" : "translate-x-1"}
+        inline-block h-4 w-4 transform
+        rounded-full bg-[#000] transition`}
+              />
+            </Switch>
           </div>
-          <div className="w-[20%]  flex items-center justify-center ">
-            Date
-          </div>
-          <div className="w-[10%]  flex items-center justify-center ">
-            Bags
-          </div>
-          <div className="w-[20%] flex items-center justify-center ">
-            Recipient
-          </div>
-          <div className="w-[30%]  flex items-center justify-center ">
-            Email
+        </header>
+
+        <div className={`${darkSide ? "text-[#ffff]" : "text-[#000]"}`}>
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-5 bg-[]">
+            <div className="flex items-center justify-center  h-[70px] bg-[] space-x-1">
+              <img src={image} alt="" className="h-9 w-9" />
+              <div>
+                <li
+                  className="items-center flex flex-wrap text-[10px]
+          text-sm  sm:text-sm text-md md:text-sm"
+                >
+                  Jack
+                </li>
+                <li
+                  className="items-center  justify-center flex flex-wrap text-[10px]
+          text-sm sm:text-sm text-md md:text-sm max-sm:hidden"
+                >
+                  Id:36752890
+                </li>
+              </div>
+            </div>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Date
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              $35,00
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Receive
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Details
+            </li>
+            <AiFillDelete className="text-[30px]" />
           </div>
         </div>
-        <div className="w-[95%] h-full bg-white mt-5 rounded-md justify-between flex items-center">
-          <div className="w-[20%]  flex items-center justify-center  text-[13px]">
-            63463891937939230
-          </div>
-          <div className="w-[20%]  flex items-center justify-center  text-[14px] ">
-            11th-Nov-23
-          </div>
-          <div className="w-[10%]  flex items-center justify-center ">
-            2
-          </div>
-          <div className="w-[20%] flex items-center justify-center ">
-            Kossyrisochukwu
-          </div>
-          <div className="w-[30%]  flex items-center justify-center text-[13px] ">
-            kossyuzoigwe@gmail.com
+        <hr className="mx-auto bg-black dark:bg-white w-full" />
+
+        <div className={`${darkSide ? "text-[#ffff]" : "text-[#000]"}`}>
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-5 bg-[]">
+            <div className="flex items-center justify-center  h-[70px] bg-[] space-x-1">
+              <img src={image} alt="" className="h-9 w-9" />
+              <div>
+                <li
+                  className="items-center flex flex-wrap text-[10px]
+          text-sm  sm:text-sm text-md md:text-sm"
+                >
+                  Jack
+                </li>
+                <li
+                  className="items-center  justify-center flex flex-wrap text-[10px]
+          text-sm sm:text-sm text-md md:text-sm max-sm:hidden"
+                >
+                  Id:36752890
+                </li>
+              </div>
+            </div>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Date
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              $35,00
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Receive
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Details
+            </li>
+            <AiFillDelete className="text-[30px]" />
           </div>
         </div>
-      </div> */}
-      <div className="flex justify-center items-center w-full ">
-        <div className="flex justify-between items-center w-[95%] mt-[14px]">
-          <table className="w-full flex justify-between mt-2">
-            <table className="max-md:hidden block">
-              <th>ID</th>
-              <tr className="text-[14px] ">654e1a5c80ef728332e40e01</tr>
-              <tr className="text-[14px] ">654e1a5c80ef728332e40e01</tr>
-              <tr className="text-[14px] ">654e1a5c80ef728332e40e01</tr>
-              <tr className="text-[14px] ">654e1a5c80ef728332e40e01</tr>
-              <tr className="text-[14px] ">654e1a5c80ef728332e40e01</tr>
-            </table>
-            <table className="">
-              <th>Date</th>
-              <tr className="text-[14px] max-sm:text-[10px]">10/11/2023</tr>
-              <tr className="text-[14px] max-sm:text-[10px]">10/11/2023</tr>
-            </table>
-            <table className="max-md:hidden block">
-              <th>Recipient</th>
-              <tr className="text-[14px] max-sm:text-[10px]">
-                Kossyrisochukwu
-              </tr>
-              <tr className="text-[14px] max-sm:text-[10px]">
-                Kossyrisochukwu
-              </tr>
-            </table>
-            <table>
-              <th>Email</th>
-              <tr className="text-[14px] max-sm:text-[10px]">
-                kossyuzoigwe@gmail.com
-              </tr>
-              <tr className="text-[14px] max-sm:text-[10px]">
-                kossyuzoigwe@gmail.com
-              </tr>
-            </table>
-            <table className="flex items-center flex-col">
-              <th>Bags</th>
-              <tr className="text-[14px] max-sm:text-[10px]">2</tr>
-              <tr className="text-[14px] max-sm:text-[10px]">2</tr>
-            </table>
-            <table className="flex items-center flex-col">
-              <th>Amount</th>
-              <tr className="text-[14px] max-sm:text-[10px]">
-                <span className="mr-1 ">₦</span>4,000
-              </tr>
-              <tr className="text-[14px] max-sm:text-[10px]">
-                <span className="mr-1 ">₦</span>4,000
-              </tr>
-            </table>
-          </table>
+        <hr className="mx-auto bg-black dark:bg-white w-full" />
+
+        <div className={`${darkSide ? "text-[#ffff]" : "text-[#000]"}`}>
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-5 bg-[]">
+            <div className="flex items-center justify-center  h-[70px] bg-[] space-x-1">
+              <img src={image} alt="" className="h-9 w-9" />
+              <div>
+                <li
+                  className="items-center flex flex-wrap text-[10px]
+          text-sm  sm:text-sm text-md md:text-sm"
+                >
+                  Jack
+                </li>
+                <li
+                  className="items-center  justify-center flex flex-wrap text-[10px]
+          text-sm sm:text-sm text-md md:text-sm max-sm:hidden"
+                >
+                  Id:36752890
+                </li>
+              </div>
+            </div>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Date
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              $35,00
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Receive
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Details
+            </li>
+            <AiFillDelete className="text-[30px]" />
+          </div>
         </div>
+        <hr className="mx-auto bg-black dark:bg-white w-full" />
+
+        <div className={`${darkSide ? "text-[#ffff]" : "text-[#000]"}`}>
+          <div className="flex justify-between items-center max-w-6xl mx-auto p-5 bg-[]">
+            <div className="flex items-center justify-center  h-[70px] bg-[] space-x-1">
+              <img src={image} alt="" className="h-9 w-9" />
+              <div>
+                <li
+                  className="items-center flex flex-wrap text-[10px]
+          text-sm  sm:text-sm text-md md:text-sm"
+                >
+                  Jack
+                </li>
+                <li
+                  className="items-center  justify-center flex flex-wrap text-[10px]
+          text-sm sm:text-sm text-md md:text-sm max-sm:hidden"
+                >
+                  Id:36752890
+                </li>
+              </div>
+            </div>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Date
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              $35,00
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Receive
+            </li>
+            <li
+              className="items-center  justify-center flex flex-wrap 
+          text-sm  sm:text-sm text-md md:text-sm"
+            >
+              Details
+            </li>
+            <AiFillDelete className="text-[30px]" />
+          </div>
+        </div>
+        <hr className="mx-auto bg-black dark:bg-white w-full" />
       </div>
     </>
   );
 };
-
 export default AdminHistoryScreen;
